7ac6a293e0d9b9c6a250f85c372e32183545535d
Cesar Yeguez <Cesar.Yeguez@gmail.com>
2024-01-22 16:35:34 -0300

    estilos mode mobile culminados 90%

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const bntMenu= document.querySelector('.icon-menu');
+const bntClose= document.querySelector('.icon-menu-close');
+const nav= document.querySelector('.nav');
+
+
+
+const show =() => {
+  nav.classList.remove('hidden');
+  bntMenu.style.display="none";
+  bntClose.style.display="block";
+};
+
+
+const hidden =() => {
+  bntClose.style.display="none";
+  bntMenu.style.display="block";
+  nav.classList.add('hidden');
+};
+
+
+bntMenu.addEventListener('click', show );
+
+bntClose.addEventListener('click', hidden);
