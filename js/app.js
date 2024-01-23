485de1ee0603411b78f2e896fc29958fa3304eb5
cyeguez <cesar.yeguez@gmail.com>
2024-01-23 12:46:20 -0300

    feat(finalizar estilos mobile modal )

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
-const bntMenu= document.querySelector('.icon-menu');
-const bntClose= document.querySelector('.icon-menu-close');
-const nav= document.querySelector('.nav');
+const bntMenu = document.querySelector(".icon-menu");
+const bntClose = document.querySelector(".icon-menu-close");
+const nav = document.querySelector(".nav");
+const modalCard = document.querySelector(".modal");
+const inputRadios = modalCard.querySelectorAll("[id]");
 
+const show = () => {
+  nav.classList.remove("hidden");
+  bntMenu.style.display = "none";
+  bntClose.style.display = "block";
+};
 
-
-const show =() => {
-  nav.classList.remove('hidden');
-  bntMenu.style.display="none";
-  bntClose.style.display="block";
+const hidden = () => {
+  bntClose.style.display = "none";
+  bntMenu.style.display = "block";
+  nav.classList.add("hidden");
 };
 
 
-const hidden =() => {
-  bntClose.style.display="none";
-  bntMenu.style.display="block";
-  nav.classList.add('hidden');
-};
+
+bntMenu.addEventListener("click", show);
+
+bntClose.addEventListener("click", hidden);
 
 
-bntMenu.addEventListener('click', show );
-
-bntClose.addEventListener('click', hidden);
